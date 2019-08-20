765e40596a327f29b7b8e65464fa45dff4a217ab
Jeremy ETIENNE <jeremyetienne@hotmail.fr>
2019-08-20 17:23:11 +0200

    dev

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,16 +30,18 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            $user->setStatus(0);
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('/login');
+            return $this->redirectToRoute('app_login');
         }
 
-        return $this->render('security/login.html.twig', [
+        return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
+ * @UniqueEntity(fields={"username"}, message="Ce pseudo existe déjà")
  */
 class User implements UserInterface, \Serializable
 {
