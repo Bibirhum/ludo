223432fdc023e93b142da128d13defcf7c8b5666
franck-lavergne <franck-lavergne@laposte.net>
2019-08-20 12:03:04 +0200

    fixtures creation for game and category

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Game;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +14,34 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+
+        for ($j = 0; $j < 10; $j++) {
+            $category = new Category();
+            $category->setName('categorie_num_'.$j);
+            $category->setDescription($faker->paragraph(6, true));
+
+            $manager->persist($category);
+
+            for ($i = 0; $i < 5; $i++) {
+                $game = new Game();
+
+                $game->setName('jeu_num_'.$j.$i);
+                $game->setImage($faker->imageUrl());
+                $game->setThumbnail($faker->imageUrl(300,225));
+                $game->setTheme($faker->sentence(12, true));
+                $game->setShortDescription($faker->sentence(24, true));
+                $game->setDescription($faker->paragraph(6, true));
+                $game->setNumPlayerMin(rand(1,2));
+                $game->setNumPlayerMax(rand(2,6));
+                $game->setDuration(rand(20,90));
+                $game->setAgeMin(rand(4,18));
+                $game->setCategory($category);
+
+                $manager->persist($game);
+            }
+        }
 
         $manager->flush();
     }
