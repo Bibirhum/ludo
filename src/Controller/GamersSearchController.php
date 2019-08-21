f189a1dc21cffbd7e8ea176d64c13e08a58181bc
Jeremy ETIENNE <jeremyetienne@hotmail.fr>
2019-08-21 17:21:39 +0200

    gamersSearch

---
+++ src/Controller/GamersSearchController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GamersSearchController extends AbstractController
+{
+    /**
+     * @Route("/gamerssearch", name="gamers_search")
+     */
+    public function index()
+    {
+        return $this->render('gamers_search/index.html.twig', [
+            'controller_name' => 'GamersSearchController',
+        ]);
+    }
+}
